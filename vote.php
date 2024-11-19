5308097eb04865adc1cdee5bb712832ba16759d4
JatinNirwann <141300023+JatinNirwann@users.noreply.github.com>
2024-11-19 23:20:59 +0530

    session checking added

--- vote.php
+++ vote.php
@@ -1,3 +1,9 @@
+<?php if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    // Redirect to login page if not logged in
+    header("Location: index.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
