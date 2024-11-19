18706d330be39a3409210fcd43f863192fe51569
JatinNirwann <141300023+JatinNirwann@users.noreply.github.com>
2024-11-19 23:20:38 +0530

    update

--- logout.php
+++ logout.php
@@ -1,9 +1,8 @@
 <?php
-session_start(); // Start the session
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
+session_start();
+session_unset();
+session_destroy();
 
-// Redirect to the homepage or login page
 header("Location: index.php");
 exit();
 ?>
